9fec46498c3e82a331378ed8e19b0eecc19a8895
dgTr3 <Edugta31@gmail.com>
2026-03-01 14:52:07 -0600

    completo

--- While_Ejercicio17/Program.cs
+++ While_Ejercicio17/Program.cs
@@ -1,1 +1,16 @@
 ﻿Console.WriteLine("contar números positivos");
+int numero;
+int contador = 0;
+Console.WriteLine("Ingrese número (0 es para salir): ");
+numero = int.Parse(Console.ReadLine());
+
+while (numero!=0)
+{
+    if (numero > 0)
+    {
+        contador++;
+    }
+    Console.WriteLine("Ingrese número");
+    numero = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Positovos"+contador);
